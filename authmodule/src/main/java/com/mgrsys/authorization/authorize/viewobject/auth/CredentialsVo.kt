69941e2ee8957af6d17148ccbeee664718a6918b
mihaylov <mihaylov@magora-systems.com>
2018-08-10 18:18:53 +0700

    add auth module classess

---
+++ authmodule/src/main/java/com/mgrsys/authorization/authorize/viewobject/auth/CredentialsVo.kt
@@ -0,0 +1,9 @@
+package com.magorasystems.pmtoolpush.screen.viewobject.auth
+
+/**
+ * Developed 2018.
+ *
+ * @author Valentin S.Bolkonsky
+ */
+data class CredentialsVo(val login: String,
+                         val password: String)
